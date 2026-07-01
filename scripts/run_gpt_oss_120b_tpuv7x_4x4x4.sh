d0e56dc10e3cffd4b3d544907e2aacd3952ae2ea
aolemila <aolemilaluo@gmail.com>
2026-07-01 16:27:06 +0800

    add jax.namedscope

--- scripts/run_gpt_oss_120b_tpuv7x_4x4x4.sh
+++ scripts/run_gpt_oss_120b_tpuv7x_4x4x4.sh
@@ -73,7 +73,9 @@ XLA_FLAGS=" \
   --xla_tpu_enable_layer_scheduler_for_dependent_collectives=true \
   --xla_tpu_enable_sparse_core_collective_offload_nd_reduce_scatter=true \
   --xla_tpu_pcie_bandwidth_multiplier=0.03 \
-  --xla_tpu_enable_multi_compute_overlap_in_layer_scheduler=true "
+  --xla_tpu_enable_multi_compute_overlap_in_layer_scheduler=true \
+  --xla_enable_custom_call_region_trace=true \
+  --xla_xprof_register_llo_debug_info=true "
 
 # MaxText Workload Overrides
 # Note: use_tokamax_gmm=False 
